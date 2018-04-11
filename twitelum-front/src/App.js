8c9033d730450147313e9a778b06e3d09291df89
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2018-04-11 18:03:37 -0300

    primeiro commit

--- twitelum-front/src/App.js
+++ twitelum-front/src/App.js
@@ -13,56 +13,78 @@ export default class App extends Component {
     constructor() {
         super()
         this.state = {
-            novoTweet: ''
+            novoTweet: '',
+            tweets: []
+        }
+
+        this.adicionaTweet = this.adicionaTweet.bind(this)
+    }
+
+    adicionaTweet(event) {
+        event.preventDefault()
+        const novoTweet = this.state.novoTweet
+        const tweetsAntigos = this.state.tweets
+
+        if (novoTweet) {
+            this.setState({
+                tweets: [novoTweet, ...tweetsAntigos],
+                novoTweet: ''
+            })
         }
     }
 
-  render() {
-    return (
-      <Fragment>
-        <Cabecalho>
-            <NavMenu usuario="@annewithlasers" />
-        </Cabecalho>
-        <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span 
-                                className={`
+    render() {
+        return (
+            <Fragment>
+                <Cabecalho>
+                    <NavMenu usuario="@annewithlasers" />
+                </Cabecalho>
+                <div className="container">
+                    <Dashboard>
+                        <Widget>
+                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                                <div className="novoTweet__editorArea">
+                                    <span
+                                        className={`
                                     novoTweet__status
                                     ${ this.state.novoTweet.length > 140
-                                        ? 'novoTweet_status--invalido' : ''}
+                                                ? 'novoTweet_status--invalido' : ''}
                                 `}>
-                                { this.state.novoTweet.length} /140
+                                        {this.state.novoTweet.length} /140
                                 </span>
-                            <textarea 
-                            className="novoTweet__editor" 
-                            value={ this.state.novoTweet }
-                            onInput={ (event) => this.setState({ novoTweet: event.target.value })}                            
-                            placeholder="O que está acontecendo?">
-                            </textarea>
-                        </div>
-                        <button className="novoTweet__envia"
-                                disabled={ this.state.novoTweet.length > 140 ? true : false }
-                                type="submit">
-                            Tweetar
+                                    <textarea
+                                        className="novoTweet__editor"
+                                        value={this.state.novoTweet}
+                                        onInput={(event) => this.setState({ novoTweet: event.target.value })}
+                                        placeholder="O que está acontecendo?">
+                                    </textarea>
+                                </div>
+                                <button className="novoTweet__envia"
+                                    disabled={this.state.novoTweet.length > 140 ? true : false}
+                                    type="submit">
+                                    Tweetar
                         </button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
-        </div>
-      </Fragment>
-    );
-  }
+                            </form>
+                        </Widget>
+                        <Widget>
+                            <TrendsArea />
+                        </Widget>
+                    </Dashboard>
+                    <Dashboard posicao="centro">
+                        <Widget>
+                            <div className="tweetsArea">
+                                        { this.state.tweets.map(
+                                            (tweetInfo, index) =>
+                                                <Tweet 
+                                                    key={tweetInfo + index}
+                                                    texto={tweetInfo} />
+                                            )
+                                        }                                
+                            </div>
+                        </Widget>
+                    </Dashboard>
+                </div>
+            </Fragment>
+        );
+    }
 }
